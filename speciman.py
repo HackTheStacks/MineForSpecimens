f0ff4c7432d449093159375aa38367ec6e34c6a3
benjlis <ben@leismart.com>
2016-11-20 06:11:14 +0000

    Added pprint_specimans() and docstrings.

--- speciman.py
+++ speciman.py
@@ -1,16 +1,27 @@
 import re
+import json
 
 SPECIMAN_REGEX='AMNH [A-Z]-[0-9]*'
 
 def find_specimans(file_name):
+    """Takes as input a text file with name file_name and returns a sorted, 
+    distinct list of AMNH specimans (as defined by SPECIMAN_REGEX) contained
+    in the file."""
     text = open(file_name)
     article = text.read()
     p = re.compile(SPECIMAN_REGEX)
     sp_list = list(set(p.findall(article)))
     sp_list.sort()
     return sp_list
-    
-speciman_list = find_specimans('N3849.txt')
-print speciman_list
 
-  
+def pprint_specimans(file_name):
+    """Calls find_specimans() with the file_name, jsonify and pretty prints 
+    the results"""
+    specimans = {}
+    specimans["File"] = file_name
+    specimans["Speciman(s)"] = find_specimans(file_name)
+    print json.dumps(specimans, indent=4, sort_keys=True)
+
+pprint_specimans('N3849.txt')
+#speciman_list = find_specimans('N3849.txt')
+#print speciman_list
