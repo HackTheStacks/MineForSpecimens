29b166104fc6583f3d3f14877ea866c5e7c19e25
benjlis <ben@leismart.com>
2016-11-20 18:05:00 +0000

    Changed dictionary access to get() to eliminate error. First version of verification checking

--- specimen.py
+++ specimen.py
@@ -2,15 +2,24 @@ import re
 import json
 import csv
 
+#SPECIMEN_REGEX='AMNH [A-Z]-[0-9]+-[0-9]+|AMNH[ |-][0-9]|'
 SPECIMEN_REGEX='AMNH [A-Z]-[0-9]+|AMNH[ |-][0-9]+'
+AMNH_REGEX='AMNH \S+'
 
+def findall_regex_match(regex, search_str):
+    p = re.compile(regex)
+    sp_list = list(set(p.findall(search_str)))
+    sp_list.sort()
+    return sp_list
+    
 def find_specimens(article):
     """Returns a sorted, distinct list of AMNH specimens (as defined by 
     specimen_REGEX) contained in the file."""
-    p = re.compile(SPECIMEN_REGEX)
-    sp_list = list(set(p.findall(article)))
-    sp_list.sort()
-    return sp_list
+    return findall_regex_match(SPECIMEN_REGEX, article)
+
+def verify_specimen_regex(file_name):
+    article = open(file_name).read()
+    return findall_regex_match(AMNH_REGEX, article)
 
 def find_specimens_file(file_name):
     """Takes as input a text file with name file_name and returns a sorted, 
@@ -27,9 +36,9 @@ def lookup_specimen_names(sp_list):
     for s in sp_list:
         psn = re.split(' |-', s[5:])
         if len(psn) == 1:
-            sp_name = collection[('', psn[0])]
+            sp_name = collection.get(('', psn[0]), None)
         else: 
-            sp_name = collection[(psn[0], psn[1])]
+            sp_name = collection.get((psn[0], psn[1]), None)
         sp_numbers_names.append((s, sp_name))
     return sp_numbers_names
 
@@ -66,5 +75,6 @@ def load_collections():
 load_collections()
 
 if __name__ == "__main__":
+    #print  verify_specimen_regex('N3849.txt')
     print json_specimens('N3849.txt')
-    print find_specimens('AMNH Collection AMNH 113352')
+    #print find_specimens('AMNH Collection AMNH 113352')
