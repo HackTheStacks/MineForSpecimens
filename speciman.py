191f7e04216d71545981296e2ba8ee1da3dadcff
benjlis <ben@leismart.com>
2016-11-20 13:32:28 +0000

    Added lookups of Latin names and support of multiple names per species number.

--- speciman.py
+++ speciman.py
@@ -14,17 +14,30 @@ def find_specimans(file_name):
     sp_list = list(set(p.findall(article)))
     sp_list.sort()
     return sp_list
+    
+def lookup_speciman_names(sp_list):
+    """Tales a list of AMNH specimans as input and produces a list of 
+    speciman number and name pairs as output"""
+    sp_numbers_names = []
+    for s in sp_list:
+        psn = re.split(' |-', s[5:])
+        if len(psn) == 1:
+            sp_name = collection[('', psn[0])]
+        else: 
+            sp_name = collection[(psn[0], psn[1])]
+        sp_numbers_names.append((s, sp_name))
+    return sp_numbers_names
 
-def pprint_specimans(file_name):
+def json_specimans(file_name):
     """Calls find_specimans() with the file_name, jsonify and pretty prints 
     the results"""
     sp_list = find_specimans(file_name)
     specimans = {}
-    specimans["File"] = file_name
-    specimans["Speciman Count"] = len(sp_list)
-    specimans["Speciman(s)"] = sp_list
-    print json.dumps(specimans, indent=4, sort_keys=True)
-    
+    specimans["file"] = file_name
+    specimans["speciman_count"] = len(sp_list)
+    specimans["speciman_list"] = sp_list
+    specimans["speciman_list_with_names"] = lookup_speciman_names(sp_list)
+    return json.dumps(specimans, indent=4, sort_keys=True)
     
 collection = {}    
 def load_collections():
@@ -32,15 +45,18 @@ def load_collections():
         collection_reader = csv.reader(csvfile)
         collection_reader.next()    # ignore header
         for row in collection_reader:
-            collection[(row[0],row[1])] = row[2:]
+            if (row[0], row[1]) in collection:
+                collection[(row[0],row[1])].append(row[2:])
+            else:
+                collection[(row[0],row[1])] = [row[2:]]
     with open('CatalogNos/bird.txt', 'rb') as csvfile:
         collection_reader = csv.reader(csvfile)
         collection_reader.next()    # ignore header
         for row in collection_reader:
-            collection[('',row[1])] = row[3:]
+            if ('', row[1]) in collection: 
+                collection[('',row[1])].append(row[3:])
+            else:    
+                collection[('',row[1])] = [row[3:]]
     
-    
-pprint_specimans('N3849.txt')
-load_collections()
-print collection[('A','10329')]
-print collection[('', '674957')]
+load_collections() 
+print json_specimans('N3849.txt')
